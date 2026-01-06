b119a28251f281d10f6764ee6c8dc124e636d6a1
jhigger <njohnjhigger@gmail.com>
2026-01-06 22:02:28 +0800

    feat: add insertion sort demo

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,46 @@
-void main() throws InterruptedException {
+void main() {
     IO.println("Hello, World!");
 
     long start = System.nanoTime();
 
     // ======= program start =======
 
-    Thread.sleep(3000);
+    int[] randomArray = generateRandomArray(10, 10);
+    System.out.println("Random Array: " + Arrays.toString(randomArray));
+
+    useInsertionSort(randomArray);
 
     // =============================
 
-    long duration =  (System.nanoTime() - start)/1000000;
-    System.out.println(duration+"ms");
+    long duration = (System.nanoTime() - start) / 1000000;
+    System.out.println(duration + "ms");
+}
+
+public static void useInsertionSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    System.out.println("Sorted Array: " + Arrays.toString(arr));
+}
+
+public static int[] generateRandomArray(int size, int maxRange) {
+    int[] randomArray = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++) {
+        // nextInt(bound) generates a number from 0 (inclusive) to bound (exclusive)
+        randomArray[i] = random.nextInt(maxRange);
+    }
+
+    return randomArray;
 }
