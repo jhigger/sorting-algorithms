a98d445c725c1543e7ad1aab4b8e7e1f1ef98fb9
jhigger <njohnjhigger@gmail.com>
2026-01-07 04:48:35 +0800

    feat: add quicksort

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,46 @@ void main() {
 
     // ======= program start =======
 
-    int[] randomArray = generateRandomArray(10, 10);
+    int[] randomArray = generateRandomArray(100, 100);
     System.out.println("Random Array: " + Arrays.toString(randomArray));
 
-    useInsertionSort(randomArray);
+//    useInsertionSort(randomArray);
+    useQuickSort(randomArray, 0, randomArray.length - 1);
+
+    System.out.println("Sorted Array: " + Arrays.toString(randomArray));
 
     // =============================
 
-    long duration = (System.nanoTime() - start) / 1000000;
+    double duration = (double) (System.nanoTime() - start) / 1000000;
     System.out.println(duration + "ms");
+}
+
+static void useQuickSort(int[] arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+        useQuickSort(arr, low, pivotIndex - 1);
+        useQuickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+static int partition(int[] arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
 }
 
 public static void useInsertionSort(int[] arr) {
@@ -29,8 +60,6 @@ public static void useInsertionSort(int[] arr) {
         }
         arr[j + 1] = key;
     }
-
-    System.out.println("Sorted Array: " + Arrays.toString(arr));
 }
 
 public static int[] generateRandomArray(int size, int maxRange) {
